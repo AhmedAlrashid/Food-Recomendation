098d0d5ced86e052e004578be8f8317087cdde34
Kelly Le <kellyl17@uci.edu>
2026-02-09 17:13:10 -0800

    Cloud Console rate limits and Google Places API minimal test

--- BE/api/main.py
+++ BE/api/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import requests, os
 
 # Create FastAPI app instance
 app = FastAPI()
@@ -13,8 +15,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+load_dotenv()
+KEY = os.getenv("GOOGLE_API_KEY")
+
+if not KEY:
+    raise RuntimeError("Google API Key needs to be set ")
+
 
 # Define a simple GET endpoint
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/places")
+def places(query: str = "restaurants in Irvine"):
+    #asdf
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+    params = {"query": query, "key": KEY}
+
+    try:
+        res = requests.get(url, params=params, timeout=5)
+        data = res.json()
+    except Exception as e:
+        raise HTTPException(500, detail=str(e))
+
+
+    if data.get("status") not in ("OK", "ZERO_RESULTS"):
+        raise HTTPException(400, detail=data.get("error_message"))
+    
+    results = data.get("results", [])
+
+    return {"results": results}
