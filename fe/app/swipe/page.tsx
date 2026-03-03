a405a4aa4ff8828dd7125b397fe70974d17fa7d2
Kelly Le <kellyl17@uci.edu>
2026-03-02 19:04:22 -0800

    Change button and styling

--- fe/app/swipe/page.tsx
+++ fe/app/swipe/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useState } from "react";
+import { FaHeart, FaHeartBroken } from "react-icons/fa";
 import image1 from "./hestu-dancing-with-koroks-in-tears-of-the-kingdom.avif";
 import image2 from "./IMG_9518.avif";
 import image3 from "./test3.jpg";
 
-const cards = [ //test cards
+const cards = [
     { id: 1, name: "McDonalds", photoRef: image1 },
     { id: 2, name: "Wendyyyss", photoRef: image2 },
     { id: 3, name: "Chipotle", photoRef: image3 },
@@ -38,51 +39,57 @@ export default function SwipePage() {
                 {nextCard && (
                     <div style={{ ...cardStyle, ...nextCardStyle }}>
                         <h2>{nextCard.name}</h2>
-                        <img
-                            src={nextCard.photoRef.src}
-                            style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "20px" }}
-                        />
+                        <img src={nextCard.photoRef.src} style={imageStyle} />
                     </div>
                 )}
 
                 <div
                     style={{
-                    ...cardStyle,
-                    ...(direction === "left" ? swipeLeft : {}),
-                    ...(direction === "right" ? swipeRight : {}),
+                        ...cardStyle,
+                        ...(direction === "left" ? swipeLeft : {}),
+                        ...(direction === "right" ? swipeRight : {}),
                     }}
                 >
-                    <h2>{currentCard.name}</h2>
-                        <img
-                            src={currentCard.photoRef.src}
-                            style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "20px" }}
-                        />
-                </div>
-            </div>
+                    <button
+                        onClick={() => handleSwipe("left")}
+                        style={leftButtonStyle}
+                    >
+                        <FaHeartBroken size={26} />
+                    </button>
 
-            <div style={{ display: "flex", gap: "20px" }}>
-                <button onClick={() => handleSwipe("left")}>Swipe Left</button>
-                <button onClick={() => handleSwipe("right")}>Swipe Right</button>
+                    <button
+                        onClick={() => handleSwipe("right")}
+                        style={rightButtonStyle}
+                    >
+                        <FaHeart size={26} />
+                    </button>
+
+                    <h2>{currentCard.name}</h2>
+
+                    <img src={currentCard.photoRef.src} style={imageStyle} />
+
+
+                </div>
             </div>
         </div>
     );
 }
 
+
 // `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photo_reference=${currentCard.photoRef}&key=API_KEY` link example
+
 
 const containerStyle: React.CSSProperties = {
     height: "100vh",
     display: "flex",
-    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    gap: "30px",
 };
 
 const stackStyle: React.CSSProperties = {
     position: "relative",
-    width: "800px",
-    height: "400px",
+    width: "1200px",
+    height: "600px",
 };
 
 const cardStyle: React.CSSProperties = {
@@ -91,12 +98,18 @@ const cardStyle: React.CSSProperties = {
     height: "100%",
     background: "white",
     borderRadius: "20px",
-    boxShadow: "0 10px 25px rgba(0,0,0,0.15)",
+    boxShadow: "0 20px 40px rgba(0,0,0,0.12)",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    overflow: "hidden",
     transition: "transform 0.3s ease, opacity 0.3s ease",
+};
+
+const imageStyle: React.CSSProperties = {
+    width: "100%",
+    height: "65%",
+    objectFit: "cover",
+    borderRadius: "20px",
 };
 
 const nextCardStyle: React.CSSProperties = {
@@ -112,4 +125,35 @@ const swipeLeft: React.CSSProperties = {
 const swipeRight: React.CSSProperties = {
     transform: "translateX(500px) rotate(20deg)",
     opacity: 0,
+};
+
+const baseButtonStyle: React.CSSProperties = {
+    position: "absolute",
+    top: "50%",
+    transform: "translateY(-50%)",
+    zIndex: 10,
+    border: "none",
+    borderRadius: "50%",
+    width: "60px",
+    height: "60px",
+    cursor: "pointer",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+};
+
+const leftButtonStyle: React.CSSProperties = {
+    ...baseButtonStyle,
+    left: "20px",
+    background: "rgba(200, 0, 0, 0.85)",
+    color: "white",
+    opacity: 0.9
+};
+
+const rightButtonStyle: React.CSSProperties = {
+    ...baseButtonStyle,
+    right: "20px",
+    background: "rgba(0, 180, 0, 0.85)",
+    color: "white",
+    opacity: 0.9
 };
